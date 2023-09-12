fe8f6bd7df7743763193d1f79ce95673734f9c65
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2023-09-13 01:04:36 +0530

    Update:

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import {BsMicrosoft} from "react-icons/bs";
+let navigation = document.querySelector(".navigation");
+let toggle = document.querySelector(".toggle");
+toggle.onclick = function () {
+  navigation.classList.toggle("active");
+};
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div class="container">
+        <div class="navigation">
+          <ul>
+            <li>
+              <a href="#">
+                <span class="icon">
+                  <BsMicrosoft size={30} color="#512B81"/>
+                </span>
+                <span class="title">Microsoft</span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <span class="icon">
+                  <ion-icon name="desktop-outline"></ion-icon>
+                </span>
+                <span class="title">Dashboard</span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <span class="icon">
+                  <ion-icon name="call-outline"></ion-icon>
+                </span>
+                <span class="title">Calls</span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <span class="icon">
+                  <ion-icon name="chatbox-outline"></ion-icon>
+                </span>
+                <span class="title">Messages</span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <span class="icon">
+                  <ion-icon name="mail-outline"></ion-icon>
+                </span>
+                <span class="title">Mails</span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <span class="icon">
+                  <ion-icon name="file-tray-stacked-outline"></ion-icon>
+                </span>
+                <span class="title">Documents</span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <span class="icon">
+                  <ion-icon name="settings-outline"></ion-icon>
+                </span>
+                <span class="title">Settings</span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <span class="icon">
+                  <ion-icon name="help-buoy-outline"></ion-icon>
+                </span>
+                <span class="title">Help</span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <span class="icon">
+                  <ion-icon name="log-out-outline"></ion-icon>
+                </span>
+                <span class="title">Sign out</span>
+              </a>
+            </li>
+          </ul>
+          <div class="toggle"></div>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
