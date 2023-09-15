f74d8c51f89b362ec8068b591190aed35af2f467
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2023-09-16 00:09:54 +0530

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -1,92 +1,92 @@
-import React from "react";
-import {BsMicrosoft} from "react-icons/bs";
-let navigation = document.querySelector(".navigation");
-let toggle = document.querySelector(".toggle");
-toggle.onclick = function () {
-  navigation.classList.toggle("active");
-};
+import React, { Component, Fragment } from "react";
+import { BsMicrosoft } from "react-icons/bs";
+import { FiPhoneCall } from "react-icons/fi";
+import { MdSpaceDashboard, MdPool } from "react-icons/md";
+import { TbMailShare } from "react-icons/tb";
+import { SlDocs } from "react-icons/sl";
+import { LuLogOut, LuActivity } from "react-icons/lu";
+import { PiHandCoinsDuotone } from "react-icons/pi";
+
+
+
+
+
+// let navigation = document.querySelector(".navigation");
+// let toggle = document.querySelector(".toggle");
+// toggle.onclick = function () {
+//   navigation.classList.toggle("active");
+// };
 const App = () => {
+ 
+
   return (
     <>
-      <div class="container">
-        <div class="navigation">
+      {/* <MergePDFs/> */}
+      {/* <InvoiceGenerator/> */}
+     
+            {/* <div className="container">
+        <div className="navigation">
           <ul>
             <li>
               <a href="#">
-                <span class="icon">
+                <span className="icon">
                   <BsMicrosoft size={30} color="#512B81"/>
                 </span>
-                <span class="title">Microsoft</span>
+                <span className="title">Home</span>
               </a>
             </li>
             <li>
               <a href="#">
-                <span class="icon">
-                  <ion-icon name="desktop-outline"></ion-icon>
+                <span className="icon">
+                  <MdSpaceDashboard size={30} color="#512B81" />
                 </span>
-                <span class="title">Dashboard</span>
+                <span className="title">Dashboard</span>
               </a>
             </li>
             <li>
               <a href="#">
-                <span class="icon">
-                  <ion-icon name="call-outline"></ion-icon>
+                <span className="icon">
+                  <MdPool size={30} color="#512B81"/>
                 </span>
-                <span class="title">Calls</span>
+                <span className="title">Pools</span>
               </a>
             </li>
             <li>
               <a href="#">
-                <span class="icon">
-                  <ion-icon name="chatbox-outline"></ion-icon>
+                <span className="icon">
+                  <PiHandCoinsDuotone size={40} color="#512B81" />
                 </span>
-                <span class="title">Messages</span>
+                <span className="title">Rewards</span>
               </a>
             </li>
             <li>
               <a href="#">
-                <span class="icon">
-                  <ion-icon name="mail-outline"></ion-icon>
+                <span className="icon">
+                  <LuActivity size={30} color="#512B81" />
                 </span>
-                <span class="title">Mails</span>
+                <span className="title">My-Activities</span>
               </a>
             </li>
             <li>
               <a href="#">
-                <span class="icon">
-                  <ion-icon name="file-tray-stacked-outline"></ion-icon>
+                <span className="icon">
+                  <SlDocs size={30} color="#512B81" />
                 </span>
-                <span class="title">Documents</span>
+                <span className="title">Docs-Verification</span>
               </a>
             </li>
             <li>
               <a href="#">
-                <span class="icon">
-                  <ion-icon name="settings-outline"></ion-icon>
+                <span className="icon">
+                  <LuLogOut size={30} color="#512B81" />
                 </span>
-                <span class="title">Settings</span>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <span class="icon">
-                  <ion-icon name="help-buoy-outline"></ion-icon>
-                </span>
-                <span class="title">Help</span>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <span class="icon">
-                  <ion-icon name="log-out-outline"></ion-icon>
-                </span>
-                <span class="title">Sign out</span>
+                <span className="title">Dis-Connect</span>
               </a>
             </li>
           </ul>
-          <div class="toggle"></div>
+          <div className="toggle"></div>
         </div>
-      </div>
+      </div> */}
     </>
   );
 };
