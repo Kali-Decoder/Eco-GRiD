45bdca9a9f40c4afd4e11f045317c3e7c0039ac8
Rkesh Roushan <rakeshroushan2341@gmail.com>
2023-09-22 01:27:20 +0530

    api testing post register-doc

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const axios = require("axios");
 const { getSHAHash } = require("../utils");
 const router = express.Router();
-const uuidv4 = require("uuid");
+// const uuidv4 = require("uuid");
 
 router.get("/get-qrcode", async (req, res) => {
   try {
@@ -29,29 +29,29 @@ router.get("/get-qrcode", async (req, res) => {
 });
 
 router.post("/register-doc", async (req, res) => {
-  const uniqueID = uuidv4();
-  const name = "Neeraj";
+  const uniqueID = "90405f99-2ad5-42a9-a653-1f32e3f7ebc2";
+  const name = "Neeraj Kumar";
   const title = "Software Engineer";
   const number = "1234567890";
   const fileURL =
-    "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf";
+    "https://drive.google.com/open?id=1wpdgxcm_kobrYS-3x9ixsrWfnFKmYHoC";
   const sendData = {
     uniqueId: uniqueID,
     fileurl: fileURL,
     metadata: `Name: ${name} || Title: ${title} || Phone: ${number}`,
     parent_delimiter: "||",
     child_delimiter: ":",
-    Ispublic: 0,
+    // Ispublic: 0,
     authorizedusers: "eco-grid@gmail.com",
-    Redirecturl: "https://www.google.com",
-    IsVerificationGatewayRequired: "true",
-    sendmetadatatoblockchain: "true",
-    metadataforblockchain: "Nothing to record",
-    isparent: 0,
-    parentid: "0xcfa038455b54714821f291814071161c9870B891",
+    // Redirecturl: "https://www.google.com",
+    // IsVerificationGatewayRequired: "true",
+    // sendmetadatatoblockchain: "true",
+    // metadataforblockchain: "Nothing to record",
+    // isparent: 1,
+    // parentid: "",
   };
 
-  try {
+  https: try {
     let response = await axios.post(
       "https://my.veridocglobal.com/api/submitdocument",
       sendData,
@@ -64,14 +64,18 @@ router.post("/register-doc", async (req, res) => {
               process.env.VERI_DOC_PRIVATE_KEY,
               sendData.uniqueId,
               sendData.fileurl,
+              // sendData.isfileurlpublickey,
               sendData.metadata,
               sendData.parent_delimiter,
               sendData.child_delimiter,
-              sendData.Redirecturl,
-              sendData.sendmetadatatoblockchain,
-              sendData.metadataforblockchain,
-              sendData.isparent,
-              sendData.parentid,
+              // sendData.Ispublic,
+              // sendData.authorizedusers,
+              // sendData.Redirecturl,
+              // sendData.IsVerificationGatewayRequired,
+              // sendData.sendmetadatatoblockchain,
+              // sendData.metadataforblockchain,
+              // sendData.isparent,
+              // sendData.parentid,
               process.env.VERI_DOC_SECRET_KEY,
             ].join("")
           ),
